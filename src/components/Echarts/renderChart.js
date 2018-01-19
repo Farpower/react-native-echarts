90e68d7bfb74f0bb3a308c7f4756a96618638855
lidahao <alwaysloseall@gmail.com>
2018-01-19 10:58:27 +0800

    add viewport meta at injectedJavaScript

--- src/components/Echarts/renderChart.js
+++ src/components/Echarts/renderChart.js
@@ -5,6 +5,10 @@ export default function renderChart(props) {
   const height = `${props.height || 400}px`;
   const width = props.width ? `${props.width}px` : 'auto';
   return `
+    var oMeta = document.createElement("meta");
+    oMeta.name = "viewport";
+    oMeta.content = "width=device-width, initial-scale=1, maximum-scale=1";
+    document.getElementsByTagName("head")[0].appendChild(oMeta);
     document.getElementById('main').style.height = "${height}";
     document.getElementById('main').style.width = "${width}";
     var myChart = echarts.init(document.getElementById('main'));
