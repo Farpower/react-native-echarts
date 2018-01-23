8c6e1cd75abb5c77de2aa2c5ff705cb33dd66990
lidahao <alwaysloseall@gmail.com>
2018-01-23 16:48:12 +0800

    add string value for width property

--- src/components/Echarts/renderChart.js
+++ src/components/Echarts/renderChart.js
@@ -2,14 +2,8 @@ import echarts from './echarts.min';
 import toString from '../../util/toString';
 
 export default function renderChart(props) {
-  const height = `${props.height || 400}px`;
-  const width = props.width ? `${props.width}px` : 'auto';
+  const width = props.width ? (typeof props.width === "number" ? `${props.width}px` : props.width) : 'auto';
   return `
-    var oMeta = document.createElement("meta");
-    oMeta.name = "viewport";
-    oMeta.content = "width=device-width, initial-scale=1, maximum-scale=1";
-    document.getElementsByTagName("head")[0].appendChild(oMeta);
-    document.getElementById('main').style.height = "${height}";
     document.getElementById('main').style.width = "${width}";
     var myChart = echarts.init(document.getElementById('main'));
     myChart.setOption(${toString(props.option)});
