e46d4db76805a8b9cd4221d7a82863652fa89ce3
lidahao <alwaysloseall@gmail.com>
2018-01-24 09:56:20 +0800

    fix some bugs

--- src/components/Echarts/renderChart.js
+++ src/components/Echarts/renderChart.js
@@ -2,8 +2,10 @@ import echarts from './echarts.min';
 import toString from '../../util/toString';
 
 export default function renderChart(props) {
+  const height = `${props.height || 400}px`;
   const width = props.width ? (typeof props.width === "number" ? `${props.width}px` : props.width) : 'auto';
   return `
+    document.getElementById('main').style.height = "${height}";
     document.getElementById('main').style.width = "${width}";
     var myChart = echarts.init(document.getElementById('main'));
     myChart.setOption(${toString(props.option)});
