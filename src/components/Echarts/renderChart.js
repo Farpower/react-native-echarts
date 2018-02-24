977c615f72130b7a9fd2c68a7a8a8887e6d387a2
vldh <alwaysloseall@gmail.com>
2018-02-24 16:26:25 +0800

    add scrollDirectory config in props

--- src/components/Echarts/renderChart.js
+++ src/components/Echarts/renderChart.js
@@ -4,9 +4,11 @@ import toString from '../../util/toString';
 export default function renderChart(props) {
   const height = `${props.height || 400}px`;
   const width = props.width ? (typeof props.width === "number" ? `${props.width}px` : props.width) : 'auto';
+  const scrollDir = props.scrollDir == 'right' ? 'document.body.scrollLeft = document.body.scrollWidth;' : '';
   return `
     document.getElementById('main').style.height = "${height}";
     document.getElementById('main').style.width = "${width}";
+    ${scrollDir}
     var myChart = echarts.init(document.getElementById('main'));
     myChart.setOption(${toString(props.option)});
     myChart.on('click', function(params) {
